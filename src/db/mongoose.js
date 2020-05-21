6727d7e204a798c0c6d646ba8f1ab626b1fc4f1b
ZIYAF MOHAMMED SADIRI <ziyafms@pop-os.localdomain>
2020-05-22 00:59:08 +0530

    added a schema for tasks with mongoose.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,54 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
-mongoose.connect('mongdb://127.0.0.1:27017/task-manager-api', {
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useUnifiedTopology: true
 })
 
+const Task = mongoose.model('Task', {
+    discription:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    // password: {
+    //     type: String,
+    //     required: true,
+    //     trim:true,
+    //     minlength: 6,
+    //     validate(value){
+    //         if(validator.contains(value, 'password')){
+    //             throw new Error('The password should not contain the word password in it')
+    //         }
+    //     }
+    // }
+})
+
+const task = new Task({
+    discription: 'Start the M word',
+    completed: true
+})
+
+task.save().then((result)=>{
+    console.log('Task successfully added')
+}).catch((error)=>{
+    console.log(error)
+})
+
+// const t1 = new Task({
+//     discription: 'Read The lean startup',
+//     completed: true,
+//     password: 'password'
+// })
+
+// t1.save().then(()=>{
+//     console.log(t1)
+// }).catch((error)=>{
+//     console.log(error)
+// })
